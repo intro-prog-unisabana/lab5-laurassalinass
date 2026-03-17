a32b9fe47986dbc63f51dd36e4b102573551e549
Laura S <laurarodrsali@unisabana.edu.co>
2026-03-16 21:07:40 -0500

    12

--- random_q.py
+++ random_q.py
@@ -1,6 +1,8 @@
-print("Enter the start value:\n")
-inicial=input()
-print("Enter the end value:\n")
-final= input()
+import random
 aleatorio= random.seed(123)
-print("Generated random number:", aleatorio)
+print("Enter the start value:\n")
+inicial=int(input())
+print("Enter the end value:\n")
+final= int(input())
+entero_aleatorio = random.randint(inicial, final)
+print("Generated random number:", entero_aleatorio)
