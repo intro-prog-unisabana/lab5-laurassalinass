c66cfb39d99650035194a005595b54ec0411f29d
Laura S <laurarodrsali@unisabana.edu.co>
2026-03-17 23:50:06 -0500

    23

--- utils_calc.py
+++ utils_calc.py
@@ -1,21 +1,61 @@
-def add(a, b):
-    return a + b
+def multiply(num1, num2):
+    result = num1*num2
+    if isinstance(num1, float) or isinstance(num2, float):
+        return float(result)
+    
+    return result
 
-def sub(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
+def divide(num1, num2):
+    if num2 == 0:
         return "Error: Division by zero is not allowed."
-    return a / b
+    
+    result = num1 / num2
+    
+    if isinstance(num1, float) or isinstance(num2, float):
+        return float(result)
+    
+    return result
 
-def exponent(a, b):
-    return a ** b
+def exponent(base, exp):
+    result = base**exp
 
-def modulo(a, b):
-    if b == 0:
-        return "Error: Modulo by zero is not allowed."
-    return a % b
+    if isinstance(base, float) or isinstance(exp, float):
+        return float(result)
+    
+    return result
+
+def modulo(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    
+    result = num1 % num2
+    
+    if isinstance(num1, float) or isinstance(num2, float):
+        return float(result)
+    
+    return result
+
+def floor_divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    
+    result = num1 // num2
+    
+    if isinstance(num1, float) or isinstance(num2, float):
+        return float(result)
+    
+    return result
+
+def absolute(num):
+    if num is int:
+        if num >=0:
+            abs = num
+        else:
+            abs = -1*num
+            
+    else:
+        if num >=0:
+            abs = num
+        else:
+            abs = -1*num
+    return abs
